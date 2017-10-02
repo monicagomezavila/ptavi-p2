a54ffaa5bb874ac8eef3656aee0c37fb5c7f79de
Monica Gomez Avila <mgavila@gamma01.aulas.gsyc.urjc.es>
2017-10-02 15:49:50 +0200

    Ya usa herencia, la división entre 0 para la división cuando se encuentra un 0. Error de meter un fichero sin poner txt sin solucionar

--- calcplus.py
+++ calcplus.py
@@ -5,55 +5,75 @@ import sys
 import calcoo
 import calcoohija
 
-try:
-    fichero = str(sys.argv[1])
-except IndexError:
-    sys.exit("Escribe el nombre del fichero, ejemplo: fichero.txt")
 
-fich = open(fichero, "r") 
+class CalculadoraPlus(calcoohija.CalculadoraHija):
+    def __init__(self, num1,num2):
+        self.valor1=num1
+        self.valor2=num2
 
 
-for linea in fich:
-    linea = linea.replace("\n","")
-    operaciones = linea.split(",")
-    elementos = len(operaciones)
-    numeros_sobran = operaciones[3:elementos]
-    
-    operando1 = int(operaciones[1])
-    operando2 = int(operaciones[2])
-  
-    if operaciones[0] == "suma":
-        result = operando1+operando2
-        for numero in numeros_sobran:
-            result = result + int(numero)
+if __name__ == "__main__":
+
+    try:
+        fichero = str(sys.argv[1])
+    except IndexError:
+        sys.exit("Escribe el nombre del fichero, ejemplo: fichero.txt")
+
+    fich = open(fichero, "r") 
+
+
+    for linea in fich:
+        linea = linea.replace("\n","")
+        operaciones = linea.split(",")
+        elementos = len(operaciones)
+        numeros_sobran = operaciones[3:elementos]
         
-        print(result)
-
-    elif operaciones[0] == "resta":
-        result = operando1-operando2
-        for numero in numeros_sobran:
-            result = result - int(numero)
-        
-        print(result)
-
-    elif operaciones[0] == "multiplica":
-        result = operando1*operando2
-        for numero in numeros_sobran:
-            result = result * int(numero)
-        
-        print(result)
-
-    elif operaciones[0] == "divide":
         try:
-           result = operando1/operando2
-        except ZeroDivisionError:
-           print("Division by zero is not allowed")         
-        for numero in numeros_sobran:
-            result = result / int(numero)
+            operando1 = int(operaciones[1])
+            operando2 = int(operaciones[2])
+        except ValueError:
+            print("Introduce valores enteros en tu fichero")
         
-        print(result)
+        numeros = CalculadoraPlus(operando1,operando2)
+  
+        if operaciones[0] == "suma":
+            result = numeros.suma()
+            for numero in numeros_sobran:
+                result = result + int(numero)
+        
+            print(result)
+
+        elif operaciones[0] == "resta":
+            result = numeros.resta()
+            for numero in numeros_sobran:
+                result = result - int(numero)
+        
+            print(result)
+
+        elif operaciones[0] == "multiplica":
+            result = numeros.mult()
+            for numero in numeros_sobran:
+                result = result * int(numero)
+        
+            print(result)
+
+        elif operaciones[0] == "divide":
+            try:
+                result = numeros.div()
+            except ZeroDivisionError:
+                print("No se puede dividir entre 0") 
+                break        
+            for numero in numeros_sobran:
+                try:
+                    result = result / int(numero)
+                except ZeroDivisionError:
+                     print("No se puede divir entre 0")
+                     print("Tu resultado antes de encontar un 0 es:")
+                     break 
+        
+            print(result)
     
-    else:
-        print('Operación sólo puede ser suma, resta, divide, multiplica.')
+        else:
+            print('Operación sólo puede ser suma, resta, divide, multiplica.')
 
 fich.close()
