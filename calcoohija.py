e62e3b70aa8533ca7778f09468f082985ea5757a
Monica Gomez Avila <mgavila@gamma01.aulas.gsyc.urjc.es>
2017-10-02 16:42:01 +0200

    Pasado por PEP8

--- calcoohija.py
+++ calcoohija.py
@@ -4,12 +4,15 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija(calcoo.Calculadora):
-    def __init__(self, num1,num2):
-        self.valor1=num1
-        self.valor2=num2
+    def __init__(self, num1, num2):
+        self.valor1 = num1
+        self.valor2 = num2
+
     def mult(self):
-       return self.valor1 * self.valor2
+        return self.valor1 * self.valor2
+
     def div(self):
         return self.valor1 / self.valor2
 
@@ -19,8 +22,8 @@ if __name__ == "__main__":
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-    
-    numeros = CalculadoraHija(operando1,operando2)
+
+    numeros = CalculadoraHija(operando1, operando2)
 
     if sys.argv[2] == "suma":
         result = numeros.suma()
@@ -30,13 +33,13 @@ if __name__ == "__main__":
         result = numeros.mult()
     elif sys.argv[2] == "divide":
         try:
-           result = numeros.div()
+            result = numeros.div()
         except ZeroDivisionError:
-           print("Division by zero is not allowed")
+            print("Division by zero is not allowed")
     else:
         sys.exit('Operación sólo puede ser suma, resta, divide, multiplica.')
 
-    try:   
-       print(result)
+    try:
+        print(result)
     except NameError:
-       print("No se puede mostrar el resultado")
+        print("No se puede mostrar el resultado")
